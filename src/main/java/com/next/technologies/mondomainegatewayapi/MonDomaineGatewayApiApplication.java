6c4b40940a2a0c21b60325e31349e0cfa0070d39
Namory Traore <namorytraore1984@yahoo.fr>
2021-12-28 23:00:25 -0500

    1er commit

---
+++ src/main/java/com/next/technologies/mondomainegatewayapi/MonDomaineGatewayApiApplication.java
@@ -0,0 +1,13 @@
+package com.next.technologies.mondomainegatewayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonDomaineGatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonDomaineGatewayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/next/technologies/mondomainegatewayapi/MonDomaineGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next.technologies.mondomainegatewayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonDomaineGatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
